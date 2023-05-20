5e190a7e327893fc9e0f23b4395877ac7949a444
Xero kun <elo48858@gmail.com>
2023-05-20 16:20:30 +0100

    `DOCS` Added docs to implemented methods

--- src/PracticeAlgorithms.java
+++ src/PracticeAlgorithms.java
@@ -3,6 +3,9 @@ public class PracticeAlgorithms {
         return (0 < x && x < 1) && (0 < y && y < 1);
     }
 
+    /**
+     * static method to return the binary string representation of an integer
+     * */
     public static String toBinaryString(int x) {
         StringBuilder binaryString = new StringBuilder();
         binaryString.append(binaryFormString(x, binaryString.toString()));
@@ -10,12 +13,18 @@ public class PracticeAlgorithms {
         return binaryString.toString();
     }
 
+    /**
+     * static recursive method that "builds" the binary string
+     * */
     private static String binaryFormString(int x, String binString) {
         if (x == 1) return binString + "1";
 
         return binaryFormString(x / 2, binString) + (x % 2);
     }
 
+    /**
+    * static method to print a "*" or " " depending on the contents in a two-dimensional array
+    * */
     public static void printTwoDmBooleanArray(boolean[][] theArray) {
         for (boolean[] row : theArray) {
             for (boolean column : row) {
